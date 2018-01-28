4eca11a32bcf64eb531f1254df8a7f7cba0e0f7a
Vadim Axelrod <vakselrod@cinimex.ru>
2018-01-28 15:16:31 +0300

    Fixes

--- localhost.sh
+++ localhost.sh
@@ -1,4 +1,4 @@
 # When running, set redirect domain for VK
 
 echo "Running VK Photo Downloader in background"
-nohup 'java -jar webnsfw-0.1.0-SNAPSHOT-jar-with-dependencies.jar "localhost"' &
+nohup java -jar webnsfw-0.1.0-SNAPSHOT-jar-with-dependencies.jar "localhost" &
