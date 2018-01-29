f3311e7d4a7528ddcf56135c2d2f47c07106df7f
Vadim Axelrod <vakselrod@cinimex.ru>
2018-01-29 18:17:41 +0300

    Messages translated to russian

--- src/main/java/co/axelrod/webnsfw/telegram/MyHotGirlfriendsBot.java
+++ src/main/java/co/axelrod/webnsfw/telegram/MyHotGirlfriendsBot.java
@@ -3,8 +3,13 @@ package co.axelrod.webnsfw.telegram;
 import co.axelrod.webnsfw.telegram.token.TokenStorageImpl;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Vadim Axelrod (vadim@axelrod.co) on 29.01.2018.
@@ -15,13 +20,29 @@ public class MyHotGirlfriendsBot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
             Long chatId = update.getMessage().getChatId();
             System.out.println("New request from Telegram bot with id: " + chatId);
+
+
+            // Create ReplyKeyboardMarkup object
+            ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+            // Create the keyboard (list of keyboard rows)
+            List<KeyboardRow> keyboard = new ArrayList<>();
+            // Create a keyboard row
+            KeyboardRow row = new KeyboardRow();
+            // Set each button, you can also use KeyboardButton objects if you need something else than text
+            row.add("Хочу горячего");
+            // Add the first row to the keyboard
+            keyboard.add(row);
+            // Set the keyboard to the markup
+            keyboardMarkup.setKeyboard(keyboard);
+
             try {
-                execute(prepareMessage(chatId, "Hello from MyGirlfriendsBot!"));
-                execute(prepareMessage(chatId, "Bot will download your girlfriends photos from VK.com, find the hottest photo and send it to you!"));
-                execute(prepareMessage(chatId, "Work in progres for now..."));
-                execute(prepareMessage(chatId, "But you can try service now at: http://52.166.68.146:4567"));
-                execute(prepareMessage(chatId, "Open this link, authorize by your VK.com account and wait for 30-60 minutes"));
-                execute(prepareMessage(chatId, "Results will be here: http://52.166.68.146:4567/results"));
+                execute(prepareMessage(chatId, "Добро пожаловать в MyHotGirlfriends!"));
+                execute(prepareMessage(chatId, "Вы авторизуетесь через VK.com, мы выгружаем фотогафии ваших подруг, отбираем самые горячие среди всех и отправляем вам!"));
+                execute(prepareMessage(chatId, "Telegram интерфейс находится в разработке..."));
+                execute(prepareMessage(chatId, "Но вы можете воспользоваться веб версией: http://52.166.68.146:4567"));
+                execute(prepareMessage(chatId, "Откройте ссылку, авторизуйтесь через ВК и ожидайте результата"));
+                execute(prepareMessage(chatId, "Через 30-60 минут результаты будут здесь: http://52.166.68.146:4567/results"));
+                execute(prepareMessage(chatId, "Авторизуйтесь здесь: http://52.166.68.146:4567", keyboardMarkup));
             } catch (TelegramApiException ex) {
                 ex.printStackTrace();
             }
@@ -30,7 +51,7 @@ public class MyHotGirlfriendsBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return "My Hot Girlfriends";
+        return "Мои горячие подруги";
     }
 
     @Override
@@ -42,6 +63,14 @@ public class MyHotGirlfriendsBot extends TelegramLongPollingBot {
         SendMessage message = new SendMessage();
         message.setChatId(chatId);
         message.setText(text);
+        return message;
+    }
+
+    private SendMessage prepareMessage(Long chatId, String text, ReplyKeyboardMarkup keyboard) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(text);
+        message.setReplyMarkup(keyboard);
         return message;
     }
 }
