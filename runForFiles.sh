723132d86a37a92cb4ed9345be188bb897f395f0
Vadim Axelrod <vakselrod@cinimex.ru>
2018-01-29 16:55:49 +0300

    open_nsfw removed and added as submodule

--- runForFiles.sh
+++ runForFiles.sh
@@ -7,6 +7,11 @@ java -jar vk-photo-downloader-0.1.0-SNAPSHOT-jar-with-dependencies.jar "$1" "$2"
 # Parsing photo and running open_nsfw
 echo "Running open-nsfw"
 echo "Results:" > results.txt
+
+echo "Copying modified classify script to open_nsfw"
+cp classify_dir.py open_nsfw
+
+cd open_nsfw
 OUTPUT="$(docker run -a stdin -a stdout --volume=$(pwd):/workspace bvlc/caffe:cpu \
         python ./classify_dir.py \
         --model_def nsfw_model/deploy.prototxt \
@@ -15,4 +20,6 @@ OUTPUT="$(docker run -a stdin -a stdout --volume=$(pwd):/workspace bvlc/caffe:cp
         | grep NSFW \
         | awk '{ print $3 " " $4 }'
         )"
+cd ..
+
 echo ${OUTPUT} >> results.txt
